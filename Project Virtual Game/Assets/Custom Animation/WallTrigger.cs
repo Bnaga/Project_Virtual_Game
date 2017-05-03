21d565ca6eea4a21fc69087e72c0d8bb3d76e76d
Robbert <Bayangannaga@hotmail.com>
2017-05-03 22:15:44 +0200

    added start menu, game mode menu and shooting

---
+++ Project Virtual Game/Assets/Custom Animation/WallTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTrigger : MonoBehaviour {
+
+	public Animator anim;
+
+	void Start () {
+
+		anim = GetComponent<Animator>();
+
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player") {
+				anim.Play("WallSweep");
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
